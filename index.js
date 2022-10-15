35e69413345a4900032375943d76ad67398a20a0
oamr <or.deeb@gmail.com>
2022-10-15 21:05:13 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function() {
+    test = document.querySelector( "#text" ).innerHTML ="This is really cool!";
+    return test;
+  });
